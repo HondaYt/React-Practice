5afcbc61d06aa6cb4c812f4534ee78a255e65c72
HondaYt <honda.yuto.04@gmail.com>
2023-09-26 03:16:25 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+import Sam001 from './sample001'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <Sam001/>
+  </React.StrictMode>,
+)
